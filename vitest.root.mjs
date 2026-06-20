46cde1848ff9643698ef64194f86be4870658875
pedro5g <pedluiz321@gmail.com>
2026-06-20 17:02:40 -0300

    chore: inital monorepo setup
    - create full project struct
    - add lint and format config
    - add vitest config
    - add build config
    - add git actions and husky config

---
+++ vitest.root.mjs
@@ -0,0 +1,26 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vitest/config";
+import { __dirname } from "./__dirname.js";
+
+export default defineConfig({
+  resolve: {
+    conditions: ["@jit/source", "default"],
+  },
+  test: {
+    projects: ["packages/*"],
+    watch: false,
+    isolate: true,
+    setupFiles: [
+      resolve(__dirname, "tests/baseline.test.ts"),
+      resolve(__dirname, "scripts/setup.ts"),
+    ],
+    typecheck: {
+      include: ["**/*.test.ts"],
+      enabled: true,
+      ignoreSourceErrors: false,
+      checker: "tsc",
+      tsconfig: "./tsconfig.json",
+    },
+    silent: true,
+  },
+});
